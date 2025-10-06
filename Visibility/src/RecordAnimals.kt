ea6f3a580c0f51a52ce8efd3c624c30f7da051da
Larry Jones <mrwizard82d1@gmail.com>
2025-10-06 14:09:53 -0500

    Client fails to compile: `private` references

---
+++ Visibility/src/RecordAnimals.kt
@@ -0,0 +1,18 @@
+private var index = 0
+
+private class Animal(val name: String)
+
+private fun recordAnimal(
+    animal: Animal
+) {
+    println("Animal $index: ${animal.name}")
+}
+
+fun recordAnimals() {
+    recordAnimal(Animal("Tiger"))
+    recordAnimal(Animal("Antelope"))
+}
+
+fun recordAnimalsCount() {
+    println("$index animals are here!")
+}
